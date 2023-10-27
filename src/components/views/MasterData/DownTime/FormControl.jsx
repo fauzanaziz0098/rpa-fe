0eb4975dc5d0e34b9ff9912ec3b710e786d977a4
Fauzan Aziz <fauzanaziz395@gmail.com>
2023-10-27 10:30:56 +0700

    crud down time

---
+++ src/components/views/MasterData/DownTime/FormControl.jsx
@@ -0,0 +1,140 @@
+import {
+    Box,
+    Button,
+    Card,
+    Grid,
+    Group,
+    PasswordInput,
+    Select,
+    TextInput,
+    Title,
+  } from "@mantine/core";
+  import { useForm } from "@mantine/form";
+  import { useEffect, useState } from "react";
+  import axiosLossTime from "@/libs/losstime/axios";
+  import { getHeaderConfigAxios } from "@/utils/getHeaderConfigAxios";
+  import { useRouter } from "next/router";
+  import { AxiosError } from "axios";
+  import Link from "next/link";
+import { showNotification } from "@mantine/notifications";
+import { IconFaceId, IconFaceIdError } from "@tabler/icons";
+  
+  export default function FormControl({ id }) {
+    const router = useRouter();
+    const [categoryLineStop, setCategoryLineStop] = useState([]);
+    const [visible, setVisible] = useState(false);
+  
+    const form = useForm({
+      initialValues: {
+        name: "",
+        categoryLineStop: "",
+      },
+    });
+  
+    useEffect(() => {
+      const fetchDataCategoryLineStop = async () => {
+        try {
+          const { data } = (await axiosLossTime.get("category-line-stop", getHeaderConfigAxios()))
+            .data;
+          setCategoryLineStop(
+            data.map((item) => ({
+              label: item.name,
+              value: item.id,
+            }))
+          );
+        } catch (error) {
+          console.log(error, "error fetch data category line stop");
+        }
+      };
+      fetchDataCategoryLineStop();
+  
+    }, []);
+
+    useEffect(() => {
+        if (id) {
+            (async (id) => {
+                try {
+                  const { data } = await axiosLossTime.get(`line-stop/${id}`, getHeaderConfigAxios()).then(item => item.data)
+                form.setValues({
+                      name: data.name,
+                      categoryLineStop: data.categoryLineStop.id,
+                    })
+                } catch (error) {
+                    console.log(error, 'error fetch category ls data');
+                }
+            })(id)
+        }
+    }, [id])
+    
+  
+    const handleSubmit = async () => {
+      try {
+          if (id) {
+              await axiosLossTime.patch(`line-stop/${id}`, form.values, getHeaderConfigAxios());
+              router.push("/master-data/down-time");
+          } else {
+              await axiosLossTime.post("line-stop", form.values, getHeaderConfigAxios());
+              router.push("/master-data/down-time");
+          }
+          showNotification({
+            title: "Successful Submit",
+            message: "Submit Success👏",
+            icon: <IconFaceId />,
+            color: "teal",
+        });
+      } catch (error) {
+        if (error instanceof AxiosError) {
+            showNotification({
+                title: "Error Attempting Authorization",
+                message: error?.response?.data?.message ?? "Connection Error",
+                icon: <IconFaceIdError />,
+                color: "red",
+            });
+
+        }
+    } finally {
+        setVisible(false);
+    }
+    };
+
+    return (
+      <div>
+          <form
+          onSubmit={form.onSubmit(() => handleSubmit())}
+          style={{ paddingTop: "10px", paddingBottom: "10px" }}
+          >
+          <Grid columns={2}>
+              <Grid.Col span={1}>
+                <TextInput
+                    label="Name"
+                    placeholder="Name"
+                    withAsterisk
+                    {...form.getInputProps("name")}
+                />
+              </Grid.Col>
+              <Grid.Col span={1}>
+                <Select
+                    label="Category Line Stop"
+                    placeholder="Select Category Line Stop"
+                    data={categoryLineStop}
+                    searchable
+                    clearable
+                    withAsterisk
+                    {...form.getInputProps("categoryLineStop")}
+                />
+              </Grid.Col>
+              <Grid.Col span={1}>
+              </Grid.Col>
+          </Grid>
+  
+          <Group position="right" mt="xl">
+              <Button color="red" component={Link} href="/master-data/user">
+              Back
+              </Button>
+              <Button type="submit">Submit</Button>
+          </Group>
+          </form>
+      </div>
+    );
+  }
+  
--- src/pages/master-data/down-time/index.jsx
+++ src/pages/master-data/down-time/index.jsx
@@ -1,107 +1,183 @@
-import { Avatar, Button, Table, Text, TextInput } from "@mantine/core";
-import { IconPencil, IconScan, IconSearch, IconTrash } from "@tabler/icons";
+import {
+  Avatar,
+  Button,
+  Table,
+  Text,
+  TextInput,
+  Container,
+  Pagination,
+  MANTINE_COLORS,
+  ScrollArea,
+  Alert,
+  Modal,
+  Card,
+} from "@mantine/core";
+import { IconAlertCircle, IconPencil, IconScan, IconSearch, IconTrash } from "@tabler/icons";
 import Layout from "../../../components/Layout/App";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import { useEffect } from "react";
+import axiosLossTime from '@/libs/losstime/axios'
+import { showNotification } from "@mantine/notifications";
+import {getHeaderConfigAxios} from '@/utils/getHeaderConfigAxios'
+import Link from "next/link";
+import { getCookie } from "cookies-next";
+import Error from "next/error";
 
-export default function DownTimePageIndex() {
+export default function DownTimePageIndex({errors}) {
+
+  if (errors) {
+    return (
+        <Card p={"xl"}>
+            <Error title={"Forbidden "} statusCode={403} />
+        </Card>
+    );
+  }
+
   const router = useRouter();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [lineStops, setLineStops] = useState([])
+  const [searchValue, setSearchValue] = useState("")
+  const [downTimeToDelete, setDownTimeToDelete] = useState(null)
+
+  const [openedDelete, setOpenedDelete] = useState(false);
+
+  const fetchLineStops = async () => {
+    try {
+      const {data} = await axiosLossTime.get(`line-stop?search=${searchValue}`, getHeaderConfigAxios())
+      setLineStops(data.data)
+    } catch (error) {
+      console.log(error, 'error fetch data lineStops');
+    }
+  }
+  useEffect(() => {
+      fetchLineStops()
+  }, [searchValue])
+
+  const itemsPerPage = 5;
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const rows = lineStops.slice(startIndex, endIndex);
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  const handleModalDelete = (id) => {
+    setOpenedDelete(true)
+    setDownTimeToDelete(id)
+  }
+
+  const handleDelete = async () => {
+    try {
+      await axiosLossTime.delete(`line-stop/${downTimeToDelete}`, getHeaderConfigAxios());
+      setOpenedDelete(false)
+      // setTimeout(() => {
+      //   setLineStops(lineStops.filter((downTime) => downTime.id !== downTimeToDelete));
+      //   showNotification({
+      //     title: "Success",
+      //     message: "Down time deleted successfully.",
+      //     color: "teal",
+      //     icon: <IconAlertCircle size={16} />,
+      //   });
+      // }, 200);
+      fetchLineStops()
+    } catch (error) {
+      showNotification({
+        title: "Failed",
+        message: error.response.message,
+        color: "red",
+        icon: <IconAlertCircle size={16} />,
+      });    }
+  }
+
   const icon = (
     <IconSearch style={{ color: "gray", width: "1rem", height: "1rem" }} />
   );
 
-  const handleHome = () => {
-    router.push("/home");
-  };
+  const searchText = (
+    <div style={{ fontWeight: 'bold' }}>Search</div>
+  )
+
   return (
     <div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <TextInput
-          mt="md"
-          //rightSectionPointerEvents="none"
-          rightSection={icon}
-          label="Search"
-          placeholder="Search"
-          style={{ width: "10rem", marginBottom: "2rem" }}
-        />
-        <Button>Create</Button>
-      </div>
-      <Table verticalSpacing={10}>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Category</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Machine</td>
-            <td>Process Problem</td>
-            <td style={{ display: "flex", gap: "5px" }}>
-              <Button color="yellow">
-                <IconPencil size={"1.2rem"} />
-              </Button>
-              <Button color="red">
-                <IconTrash size={"1.2rem"} />
-              </Button>
-            </td>
-          </tr>
-          <tr>
-            <td>Material</td>
-            <td>Logistic Problem</td>
-            <td style={{ display: "flex", gap: "5px" }}>
-              <Button color="yellow">
-                <IconPencil size={"1.2rem"} />
-              </Button>
-              <Button color="red">
-                <IconTrash size={"1.2rem"} />
-              </Button>
-            </td>
-          </tr>
-          <tr>
-            <td>Man power</td>
-            <td>HR Problem</td>
-            <td style={{ display: "flex", gap: "5px" }}>
-              <Button color="yellow">
-                <IconPencil size={"1.2rem"} />
-              </Button>
-              <Button color="red">
-                <IconTrash size={"1.2rem"} />
-              </Button>
-            </td>
-          </tr>
-          <tr>
-            <td>Jig process welding</td>
-            <td>Process Problem</td>
-            <td style={{ display: "flex", gap: "5px" }}>
-              <Button color="yellow">
-                <IconPencil size={"1.2rem"} />
-              </Button>
-              <Button color="red">
-                <IconTrash size={"1.2rem"} />
-              </Button>
-            </td>
-          </tr>
-          <tr>
-            <td>Packing Standar</td>
-            <td>Logistic Problem</td>
-            <td style={{ display: "flex", gap: "5px" }}>
-              <Button color="yellow">
-                <IconPencil size={"1.2rem"} />
-              </Button>
-              <Button color="red">
-                <IconTrash size={"1.2rem"} />
-              </Button>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
+        <Modal
+          opened={openedDelete}
+          onClose={() => setOpenedDelete(false)}
+          title="Are you sure want to delete this?"
+          // centered
+        >
+          <div style={{ display: 'flex', justifyContent:'center', gap: '1.2rem', padding: '1rem' }}>
+            <Button onClick={() => setOpenedDelete(false)}>Cancel</Button>
+            <Button onClick={handleDelete} color="red">Delete</Button>
+          </div>
+      </Modal>
+      <ScrollArea>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <TextInput
+            mt="md"
+            //rightSectionPointerEvents="none"
+            rightSection={icon}
+            label={searchText}
+            placeholder="Search"
+            style={{ width: "10rem", marginBottom: "2rem" }}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+          <div style={{ display: 'flex', gap: '10px' }}>
+            <Button>
+              <Link style={{ textDecoration: 'none', color: 'white' }} href="/master-data/down-time/create">Create</Link>
+            </Button>
+          </div>
+        </div>
+        <Table verticalSpacing="xs" fontSize="xs" highlightOnHover>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Category</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {lineStops.length == 0 ? (
+              <tr>
+                  <td colSpan={'100%'}>
+                      <Alert icon={<IconAlertCircle size={16} />} title="Data is empty" color="gray" variant="filled">
+                          data kosong atau tidak tersedia
+                      </Alert>
+                  </td>
+              </tr>
+            ) : 
+            rows?.map((item, index) => (
+              <tr key={index}>
+                <td>{item?.name}</td>
+                <td>{item?.categoryLineStop?.name}</td>
+                <td style={{ display: "flex", gap: "5px" }}>
+                  <Button color="yellow" component={Link} href={`/master-data/down-time/${item.id}`}>
+                    <IconPencil size={"1.2rem"} />
+                  </Button>
+                  <Button onClick={() => handleModalDelete(item.id)} color="red">
+                    <IconTrash size={"1.2rem"} />
+                  </Button>
+                </td>
+              </tr>
+            ))
+            }
+          </tbody>
+        </Table>
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+          <Pagination
+            total={Math.ceil(lineStops.length / itemsPerPage)}
+            value={currentPage}
+            onChange={handlePageChange}
+          />
+        </div>
+      </ScrollArea>
     </div>
   );
 }
@@ -109,3 +185,19 @@ export default function DownTimePageIndex() {
 DownTimePageIndex.getLayout = (page) => {
   return <Layout>{page}</Layout>;
 };
+
+export async function getServerSideProps({req, res}) {
+  if (!getCookie("permissions", { req, res }).split(",")?.includes("READ:USER")) {
+    return {
+          props: {
+              errors: true,
+          },
+      };
+  } else {
+      return {
+          props: {
+              errors: false,
+          },
+      };
+  }
+}
