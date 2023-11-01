00e196211fff11d1e3778ea1c3a5337b38b30ea0
Adams <72082223+AdamDarusman@users.noreply.github.com>
2023-11-01 17:22:48 +0700

    noPlan & waktu berbarengan

--- src/pages/master-data/monitor-o-e-e/index.jsx
+++ src/pages/master-data/monitor-o-e-e/index.jsx
@@ -35,17 +35,23 @@ export default function Home() {
     const [mqttData2, setMqttData2] = useState([])
 
     const [activePlan, setActivePlan] = useState([])
+    const [shift, setShift] = useState([])
     const [noPlanToday, setNoPlanToday] = useState([])
 
     console.log(mqttData1, 'mqdata1');
     console.log(mqttData2, 'mqdata2');
 
     console.log(activePlan, 'activeplan');
+    console.log(noPlanToday, 'hsgnoplan');
+    console.log(shift, 'shift');
+
 
     const fetchActiveData = async () => {
         try {
             const res1 = await axiosPlanning.get('planning-production', getHeaderConfigAxios())
             setActivePlan(res1.data.data)
+            const res2 = await axiosPlanning.get('report-shift', getHeaderConfigAxios())
+            setShift(res2.data.data)
         } catch (error) {
             console.log(error, 'error fetch data');
         }
@@ -63,7 +69,7 @@ export default function Home() {
                 .on("message", (topic, message) => {
                     if (topic == `MC${activePlan.machine.id}:LS:RPA`) {
                         setMqttData2(JSON.parse(message))
-                        console.log('message got');
+                        console.log(message, 'message got');
                     }
                     if (topic == `MC${activePlan.machine.id}:PLAN:RPA`) {
                         setMqttData1(JSON.parse(message))
@@ -78,6 +84,34 @@ export default function Home() {
         }
     }, [activePlan])
 
+    useEffect(() => {
+        if (activePlan) {
+            try {
+                const fetchNoPlan = async () => {
+                    const res2 = await axiosPlanning.get('no-plan-machine', getHeaderConfigAxios())
+                    console.log(res2.data.data, moment().format('dddd').toLowerCase(), 'hahah');
+                    const noPlanTdy = res2.data.data.filter(item => {
+                        console.log(item, 'hahah');
+                        if (item.day == moment().format('dddd').toLowerCase()) {
+                            const timeInNoPlan = moment(item.time_in, 'HH:mm:ss')
+                            const timeOutNoPlan = moment(item.time_out, 'HH:mm:ss')
+                            const timeNow = moment(moment().format('HH:mm:ss'), 'HH:mm:ss')
+                            const timeStart = moment(moment(activePlan?.date_time_in).format('HH:mm:ss'), 'HH:mm:ss')
+        
+                            // if (timeInNoPlan.isBetween(timeStart, timeNow) && timeOutNoPlan.isBetween(timeStart, timeNow) ) {
+                                return item
+                            // }
+                        }
+                    })
+                    console.log(noPlanTdy,'notoday');
+                    setNoPlanToday(noPlanTdy)
+                }
+                fetchNoPlan()
+            } catch (error) {
+                console.log(error, 'error fetch noplan');
+            }
+        }
+    }, [activePlan])
 
     useEffect(() => {
         fetchActiveData()
@@ -235,9 +269,12 @@ const quality = calculateQuality();
         };
         calculatePlannedAvailability();
         const intervalId = setInterval(() => {
-            calculatePlannedAvailability();
-            // console.log(calculateAvailability, 'inter');
-        }, 1000);
+            setPlannedAvailability((prevTimeActual) => prevTimeActual + 1);
+          }, 60000);
+        // const intervalId = setInterval(() => {
+        //     calculatePlannedAvailability();
+        //     // console.log(calculateAvailability, 'inter');
+        // }, 1000);
 
         return () => {
             clearInterval(intervalId);
@@ -259,24 +296,42 @@ const quality = calculateQuality();
             // }
             
             newPlannedActual = timeActual;
-let noPlnaTempry=0
-            if (activePlan.shift.no_plan_machine_id) {
-                console.log(activePlan.shift.no_plan_machine_id, 'total');
-                const totalNoPlan = activePlan.shift.no_plan_machine_id.reduce((total, value) => total ?? 0 + value.total, 0);
-                noPlnaTempry = totalNoPlan ?? 0;
+            let noPlnaTempry = 0
+            console.log(noPlanToday, 'notempo');
+            if (noPlanToday.length > 0) {
+                noPlanToday.map(item => {
+                    if (
+                        moment(item.time_in, 'HH:mm:ss').isSameOrAfter(moment(moment(activePlan.date_time_in).format('HH:mm:ss'), 'HH:mm:ss')) && 
+                        moment(item.time_in, 'HH:mm:ss').isSameOrBefore(moment(moment().format('HH:mm:ss'), 'HH:mm:ss'))
+                    ) {
+                        if (item.time_out) {
+                            noPlnaTempry += (item.total ?? 0) / 2;
+                        } 
+                        // else {
+                            // noPlnaTempry += item.total ?? 0;
+                        // }
+                        console.log(noPlnaTempry, 'hsgh8');
+                    }
+                })
             }
-
+            console.log(noPlnaTempry, 'notempo', noPlanToday);
             newPlannedActual = Math.max(newPlannedActual, 0);
             newPlannedActual -= mqttData2.TotalTime;
-            console.log(mqttData2, 'total time', timeDifference, newPlannedActual);
-            setPlannedActual(timeDifference-mqttData2.TotalTime -noPlnaTempry ?? 0);
+            console.log(noPlnaTempry, 'no');
+            setPlannedActual(timeDifference-mqttData2.TotalTime - noPlnaTempry ?? 0);
+            console.log(timeDifference, mqttData2.TotalTime, activePlan.shift.no_plan_machine_id, 'ava1');
         };
 
-        calculatePlannedActual();
+        
 
+        calculatePlannedActual();
         const intervalId = setInterval(() => {
-            calculatePlannedActual();
-        }, 600000);
+            calculatePlannedActual((prevTimeActual) => prevTimeActual + 1);
+          }, 60000);
+
+        // const intervalId = setInterval(() => {
+        //     calculatePlannedActual();
+        // }, 600000);
 
         return () => {
             clearInterval(intervalId);
